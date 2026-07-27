15bff94cb29a5b6d473dc9696bcb995ffd840bb4
Nicholas Perez <Nicholasperez8@csu.fullerton.edu>
2026-07-26 20:49:04 -0700

    First commit with introduction python basics.

---
+++ introduction.py
@@ -0,0 +1,16 @@
+# This is how you comment
+# Multi-line comments
+# Use the same hash
+
+print("Hello, this is my first python code.")  # built-in function
+print('Hello, this is my first python code.')  # Single quotes
+print('\t Hello')  # inserts tab
+print("Hello 'Nicholas' how are you")  # using quote within a quote
+print("Message1\nMessage2")  # \n creates new line
+print("\nMessage3")
+
+# Creating multiline print statment with special characters
+print("""Your Learning Path:
+\t - Python Basics
+\t - Data Engineering
+\t - AI""")
