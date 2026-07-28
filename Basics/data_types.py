7a773180852518cb2626328d9ed87ac0be37603f
nicholasperez142002 <nicholasperez1402@gmail.com>
2026-07-27 22:41:48 -0700

    added data types

---
+++ Basics/data_types.py
@@ -0,0 +1,11 @@
+# Data Types
+a = 10 # int
+b = 3.15 # float
+c = "Hello" # str
+d = 'Hi'    # str
+e = "1234"  # str
+f = True    # Boolean
+g = False   # Boolean
+h = None    # NoneType
+i = ""      # str - Blank
+j = " "     # str - Empty Space
