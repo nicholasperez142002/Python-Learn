f2e00ddaa33cf70affbf8eb24b6672c201c64937
nicholasperez142002 <nicholasperez1402@gmail.com>
2026-07-30 10:14:36 -0700

    added a small formatting change to function.py

---
+++ Basics/functions.py
@@ -0,0 +1,11 @@
+#Functions
+
+#value -> Function -> output
+
+text = "Nicholas"
+number = 10
+
+print(number.bit_length())
+print(text.upper()) 
+print(len(text)) # nested function
+#print(len(number)) does not work because len is specific to str class
