70118b1e4003b880b4a8c81f73ae30330ddb9c0b
nicholasperez142002 <nicholasperez1402@gmail.com>
2026-08-03 14:49:32 -0700

    adding all unsaved files

---
+++ functions/functions.py
@@ -0,0 +1,37 @@
+#User defined functions
+
+#Function Declaration
+# def function_name(parameter):
+#   <line of code>
+
+#Function Call
+# function_name(argument)
+
+def make_coffee():
+    print("start Machine")
+    print('Make coffee')
+    print('Add milk')
+    print('Enjoy it')
+print('Wake up')
+make_coffee()
+print('Work for a while\n')
+
+import math
+#built in
+print(len('Python'))
+number = 4.2
+#import
+print(math.ceil(number))
+#user-defined
+def greet():
+    print("hello")
+greet()
+
+#Function work
+
+def clean_text(name):
+    print(name.strip().lower())
+clean_text(' Nicholas  ')
+
+
+
--- loops/challenge2.py
+++ loops/challenge2.py
@@ -1,13 +1,14 @@
 #allow up to 3 attempts
 #if the user types 'yes', print 'glad we are on the same page'
 #otherwise, print '3 strikes, you are out'
-count = 0
-while count < 3:
-    answer = input("Do you agree? (yes/no): ")
-    if answer == "yes":
-        print('glad we are on the same page')
+
+attempts = 0 #starting at 0 attempts
+while attempts < 3: #condition 
+    answer = input("Do you agree? (yes/no): ") #prompts question immediately
+    if answer == "yes": #condition 
+        print('glad we are on the same page') #print before break if yes
         break
-    count += 1
+    attempts += 1 #adds one to our attempts counter if not yes
 else:
     print('3 Strikes, you are out')
 
